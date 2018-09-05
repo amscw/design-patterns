8c66535d8d2fa7cd3892ed60bf3e7b1a21f8c759
amscw <moskwin.a.va@gmail.com>
2018-09-05 14:30:31 +0300

    factory-method demo

---
+++ ex00-factory-method/src/army.cpp
@@ -0,0 +1,35 @@
+/*
+ * army.cpp
+ *
+ *  Created on: 5 сент. 2018 г.
+ *      Author: moskw
+ */
+#include <iostream>
+#include "tracers.hpp"
+#include "army.hpp"
+
+
+army_c::army_c(const std::string &name, std::uint32_t count, force_t type) noexcept :
+	m_strName(name), m_nAmount(count), m_forceType(type)
+{
+	// common create actions
+	std::ostringstream oss;
+	oss << "creating " << m_nAmount << " " << forceTypeToString(m_forceType) << " warriors \"" << m_strName << "\"...";
+	TRACE(oss);
+	// ... nothing interest here
+}
+
+void army_c::MoveToPosition(std::uint32_t xCoord, std::uint32_t yCoord) noexcept
+{
+	std::ostringstream oss;
+	oss << m_strName << ": we are moving to (" << xCoord << ", " << yCoord << ")";
+	TRACE(oss);
+}
+
+void army_c::Formation(combatFormation_t formation) noexcept
+{
+	std::ostringstream oss;
+	oss << "we formation to " << formationToString(formation);
+	TRACE(oss);
+}
+
---
+++ ex00-factory-method/src/factory-method.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : factory-method.cpp
+// Author      : moskvin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "army.hpp"
+
+int main() {
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
